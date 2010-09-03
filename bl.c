dfd01dedc71c9336f923c22d71b4f2da52f537de
Christophe Delord <christophe.delord@free.fr>
2010-09-03 11:16:34 +0200

    Missing bl.c added in the archive

---
+++ bl.c
@@ -0,0 +1,58 @@
+/* BonaLuna
+
+Copyright (C) 2010 Christophe Delord
+http://cdsoft.fr/bonaluna.html
+
+BonaLuna is based on Lua 5.2 work 4
+Copyright (C) 2010 Lua.org, PUC-Rio.
+
+Freely available under the terms of the Lua license.
+*/
+
+#define MAKE_LUA
+#define luaall_c
+
+/* core */
+#include "lapi.c"
+#include "lcode.c"
+#include "lctype.c"
+#include "ldebug.c"
+#include "ldo.c"
+#include "ldump.c"
+#include "lfunc.c"
+#include "lgc.c"
+#include "llex.c"
+#include "lmem.c"
+#include "lobject.c"
+#include "lopcodes.c"
+#include "lparser.c"
+#include "lstate.c"
+#include "lstring.c"
+#include "ltable.c"
+#include "ltm.c"
+#include "lundump.c"
+#include "lvm.c"
+#include "lzio.c"
+
+/* auxiliary library */
+#include "lauxlib.c"
+
+/* standard library  */
+#include "lbaselib.c"
+#include "lbitlib.c"
+#include "lcorolib.c"
+#include "ldblib.c"
+#include "liolib.c"
+#include "lmathlib.c"
+#include "loadlib.c"
+#include "loslib.c"
+#include "lstrlib.c"
+#include "ltablib.c"
+#include "linit.c"
+
+/* BonaLuna libraries */
+#include "bonaluna.c"
+
+/* lua */
+#include "lua.c"
+
